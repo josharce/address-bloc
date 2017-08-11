2406e438c604ebb46f917bf539ca4c40363164e0
Josh Arce <contact@josharce.io>
2017-08-10 23:17:35 -0700

    assignment-6-csv: complete
    Create entries_2.csv and validate if import_from_csv method loads correct information from both entries.csv and entries_2.csv.

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -39,46 +39,68 @@ RSpec.describe AddressBook do
     end
 
     describe "#import_from_csv" do
-        it "imports the correct number of entries" do
+        it "imports the correct number of entries from two csv files" do
             book.import_from_csv("entries.csv")
+            book.import_from_csv("entries_2.csv")
             book_size = book.entries.size
 
-            expect(book_size).to eq 5
+            expect(book_size).to eq 8
         end
 
-        it "imports the 1st entry" do
+        it "imports the 1st entry from the first csv" do
             book.import_from_csv("entries.csv")
             entry_one = book.entries[0]
 
             check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
         end
 
-        it "imports the 2nd entry" do
+        it "imports the 2nd entry from the first csv" do
             book.import_from_csv("entries.csv")
             entry_two = book.entries[1]
 
             check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
         end
 
-        it "imports the 3rd entry" do
+        it "imports the 3rd entry from the first csv" do
             book.import_from_csv("entries.csv")
             entry_three = book.entries[2]
 
             check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
         end
 
-        it "imports the 4th entry" do
+        it "imports the 4th entry from the first csv" do
             book.import_from_csv("entries.csv")
             entry_four = book.entries[3]
 
             check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
         end
 
-        it "imports the 5th entry" do
+        it "imports the 5th entry from the first csv" do
             book.import_from_csv("entries.csv")
             entry_five = book.entries[4]
 
             check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+        end
+
+        it "imports the 1st entry from second csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+
+            check_entry(entry_one, "John", "111-111-1111", "john@blocmail.com")
+        end
+
+        it "imports the 2nd entry from second csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_two = book.entries[1]
+
+            check_entry(entry_two, "Paul", "111-111-1112", "paul@blocmail.com")
+        end
+
+        it "imports the 3rd entry from second csv" do
+            book.import_from_csv("entries_2.csv")
+            entry_three = book.entries[2]
+
+            check_entry(entry_three, "Steven", "111-111-1113", "steven@blocmail.com")
         end
     end
 end
